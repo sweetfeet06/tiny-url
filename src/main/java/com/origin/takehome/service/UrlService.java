251e348a2201fad674cff32abedaf4ae7c7b015b
Warren Sweetman <wsweetman@mac.com>
2026-03-26 11:03:01 +1100

    pass service test

--- src/main/java/com/origin/takehome/service/UrlService.java
+++ src/main/java/com/origin/takehome/service/UrlService.java
@@ -3,18 +3,30 @@ package com.origin.takehome.service;
 import org.springframework.stereotype.Service;
 
 import com.origin.takehome.domain.ShortUriMap;
+import com.origin.takehome.exception.NotFoundException;
+import com.origin.takehome.repository.ShortUriRepository;
 
 @Service
 public class UrlService {
+    
+    private ShortUriRepository repository;
 
-    public ShortUriMap shorten(String originalUrl) {
-        // TODO Auto-generated method stub
-        return null;
+    public UrlService(ShortUriRepository repository) {
+        this.repository = repository;
     }
 
-    public ShortUriMap expand(String shortUri) {
-        // TODO Auto-generated method stub
-        return null;
+    public ShortUriMap shorten(String originalUrl) {
+        var shortUri = new ShortUriMap(originalUrl);
+        repository.save(shortUri);
+        return shortUri;
+    }
+
+    public ShortUriMap expand(String id) {
+        var shortUri = repository.findById(id);
+        if (shortUri.isEmpty()) {
+            throw new NotFoundException("No shortUri for [" + id + "]");
+        }
+        return shortUri.get();
     }
 
 }
